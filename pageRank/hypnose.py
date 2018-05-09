daeb34b3f4da65c82754c3091d2f277a4d942f3b
Axel <axel.thavisouk@gmail.com>
2018-05-09 17:20:37 +0200

    algo et génération de graphes

--- pageRank/hypnose.py
+++ pageRank/hypnose.py
@@ -1,8 +1,16 @@
-successeurs = [[],[0,2],[3],[1],[0,5],[6],[4]]
-predecesseur = [[1,4],[3],[1],[2],[6],[4],[5]]
+successeurs = [[1,2],[2],[0],[2]]
 N=len(successeurs)
 d=4/5
 score = [1/N]*N
+
+def getPredecessor(succ):
+    pred=[]
+    for i in range(len(succ)):
+        pred.append([])
+    for i,l in enumerate(succ):
+        for j in l:
+            pred[j].append(i)
+    return pred
 
 def calcScore(pred, succ, current):
     sum=0
@@ -16,9 +24,12 @@ def algo(succ, pred):
     for i in range(0,30):
         for j in range(0,N):
             score[j] = calcScore(pred, succ, j)
+    total=0
     for i in range(0,N):
         print(i,":", score[i])
-
+        total+=score[i]
+    print(total)
 
 if __name__ == '__main__':
+    predecesseur = getPredecessor(successeurs)
     algo(successeurs,predecesseur)

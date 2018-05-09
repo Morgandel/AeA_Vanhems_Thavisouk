42fabd9ae8417cc222e4a21de5b1695e2908f85a
Florian Vanhems <florian.vanhems@etudiant.univ-lille1.fr>
2018-05-09 18:21:29 +0200

    Working version of rocket

--- rocket/src/pareto_front.cpp
+++ rocket/src/pareto_front.cpp
@@ -14,18 +14,38 @@ ParetoFront::ParetoFront(unsigned int n) {
 std::vector<Solution> *ParetoFront::getParetoFront() {
     this->pareto_front.emplace_back();
 
+    /*
+    std::cout << "initial pareto : ";
+    for (Solution &solution : this->pareto_front) {
+        std::cout << "(" << solution.getWeight() << ", " << solution.getProfit() << ")";
+    }
+    std::cout << "\n";
+    */
     // maybe sort the experiences here
     for (Experience &experience : this->experiences) {
-        std::cout << "getting exp : (" << experience.getWeight() << ", " << experience.getProfit() << ")\n";
+        // std::cout << "getting exp : (" << experience.getWeight() << ", " << experience.getProfit() << ")\n";
         std::vector<Solution> l1;
         std::vector<Solution> l2;
         for (Solution solution : this->pareto_front) {
             l1.push_back(solution);
             l2.emplace_back(solution, &experience);
         }
+        /*
+        std::cout << "l1 : ";
+        for (Solution &solution : l1) {
+            std::cout << "(" << solution.getWeight() << ", " << solution.getProfit() << "), ";
+        }
+        std::cout << "\n";
+        std::cout << "l2 : ";
+        for (Solution &solution : l2) {
+            std::cout << "(" << solution.getWeight() << ", " << solution.getProfit() << "), ";
+        }
+        std::cout << "\n";
+        */
         // merging l1 & l2
         this->pareto_front.erase(this->pareto_front.begin(),
                                  this->pareto_front.end());
+        // std::cout << "erasure done" << "\n";
         auto l1_it = l1.begin();
         auto l2_it = l2.begin();
         while (l1_it != l1.end() && l2_it != l2.end()) {
@@ -51,14 +71,28 @@ std::vector<Solution> *ParetoFront::getParetoFront() {
                 }
             }
         }
-        while (l1_it != l1.end())
+        // std::cout << "got to the end of a list" << "\n";
+        while (l1_it != l1.end()) {
             this->pareto_front.push_back(*l1_it);
-        while (l2_it != l2.end())
-            this->pareto_front.push_back(*l2_it);
-        std::cout << "pareto front : " << "\n";
-        for (Solution &solution : this->pareto_front) {
-            std::cout << "(" << solution.getWeight() << "," << solution.getProfit() << ")" << "\n";
+            l1_it++;
         }
+        while (l2_it != l2.end()) {
+            this->pareto_front.push_back(*l2_it);
+            l2_it++;
+        }
+        /*
+        std::cout << "pareto front : ";
+        for (Solution &solution : this->pareto_front) {
+            std::cout << "(" << solution.getWeight() << "," << solution.getProfit() << "), ";
+        }
+        std::cout << "\n";
+        */
     }
+    std::cout << "pareto front : ";
+    for (Solution &solution : this->pareto_front) {
+        std::cout << "(" << solution.getWeight() << "," << solution.getProfit() << "), ";
+    }
+    std::cout << "\n";
+    std::cout << this->pareto_front.size() << "\n";
     return &this->pareto_front;
 }
